1bda3fec3d7baafafe6b339658d3d10dbea7f9f2
verozroke <verozroke@users.noreply.github.com>
2023-04-17 12:35:05 +0000

    deploy: cfcfd94e55f73be686d22af7f7d3e9a3daa9dece

--- assets/InputStore-2423b21a.js
+++
@@ -1,1 +0,0 @@
-import{h as t}from"./index-ec07963f.js";const o=t("inputStore",{state:()=>({workTime:null,restTime:null}),actions:{changeWorkTime(e){this.workTime=e},changeRestTime(e){this.restTime=e}}});export{o as u};
